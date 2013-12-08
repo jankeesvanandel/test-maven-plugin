97ffe6b286658d82d6fd997fcdfc1e068b94edd6
Jan-Kees van Andel <jankeesvanandel@gmail.com>
2013-12-08 22:54:22 +0100

    Test with Jython mojo

---
+++ src/main/java/nl/jkva/CommandLineMojo.java
@@ -0,0 +1,7 @@
+package nl.jkva;
+
+public class CommandLineMojo {
+    public int execute(String cmd) {
+
+    }
+}
---
+++ src/main/java/nl/jkva/MavenProjectFaker.java
@@ -0,0 +1,7 @@
+package nl.jkva;
+
+/**
+ * Created by jankeesvanandel on 08-12-13.
+ */
+public class MavenProjectFaker {
+}
